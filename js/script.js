0f50df9816179cec30ce989dac6988d32111d3fe
Maxim Razin <blvckonej@gmail.com>
2022-05-09 01:05:06 +0300

    added burger menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $('.leasing-slider-item').on('click', function() {
     $(this).addClass('active').siblings().removeClass('active');
 })
+
+const menuBtn = document.querySelector('.header-content__burger');
+const menuContent = document.querySelector('.header-content__burger-menu');
+
+const closeMenu = ({ target }) => {
+
+    if (!target.closest('.header-content__burger-menu, .header-content__burger')) {
+        menuBtn.classList.remove('active');
+    menuContent.classList.remove('active-burger');
+
+    document.body.removeEventListener('click', closeMenu);
+ }
+}
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    menuContent.classList.toggle('active-burger');
+    document.body.addEventListener('click', closeMenu);
+})
