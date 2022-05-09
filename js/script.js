1671db5362c63928602db1ae302297cee0a28e40
Maxim Razin <blvckonej@gmail.com>
2022-05-09 19:32:07 +0300

    added owl carousel

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-$('.leasing-slider-item').on('click', function() {
+$('.leasing-slider-item').on('click', function () {
     $(this).addClass('active').siblings().removeClass('active');
 })
 
 const menuBtn = document.querySelector('.header-content__burger');
 const menuContent = document.querySelector('.header-content__burger-menu');
 
-const closeMenu = ({ target }) => {
+const closeMenu = ({
+    target
+}) => {
 
     if (!target.closest('.header-content__burger-menu, .header-content__burger')) {
         menuBtn.classList.remove('active');
-    menuContent.classList.remove('active-burger');
+        menuContent.classList.remove('active-burger');
 
-    document.body.removeEventListener('click', closeMenu);
- }
+        document.body.removeEventListener('click', closeMenu);
+    }
 }
 
 menuBtn.addEventListener('click', () => {
@@ -20,3 +22,28 @@ menuBtn.addEventListener('click', () => {
     menuContent.classList.toggle('active-burger');
     document.body.addEventListener('click', closeMenu);
 })
+
+
+
+$(window).resize(function () {
+    if ($(window).width() < 768) {
+        $(document).ready(function () {
+            $(".owl-carousel").owlCarousel({
+                items: 2,
+                stagePadding: 50,
+                responsive: {
+                    // breakpoint from 480 up
+                    375: {
+                        items: 1,
+                        stagePadding: 30,
+                    },
+                    480: {
+                        items: 2,
+                        stagePadding: 50,
+                    }
+                }
+
+            });
+        });;
+    }
+});
